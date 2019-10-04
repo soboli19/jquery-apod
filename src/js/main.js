dffb9a23d7bf7b5f70aed0e89f9c2013d392bccf
ILIYA SOBOLEV <soboli@att.net>
2019-10-04 13:54:54 -0500

    Jquery API fixed

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+var apod = {
+  //Create a random date
+  randomDate: function(start, end) {
+    //Randomize the date https://gist.github.com/miguelmota/5b67e03845d840c949c4
+    let date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+
+    //Format the date
+    let d = date.getDate();
+    let m = date.getMonth() + 1; //In JS months start at 0
+    let y = date.getFullYear();
+
+    //Change the month and day strings so that they match the documented format.
+    if(m < 10){
+      m = '0'+m
+    }
+
+    if(d < 10){
+      d = '0'+d
+    }
+
+    return `${y}-${m}-${d}`;
+  },
+
+  // Application Constructor
+  init: function() {
+    let date = this.randomDate(new Date(1995, 5, 16), new Date());
+    var url = "https://api.nasa.gov/planetary/apod?api_key=BXhedIXLZAtpimXW4995CMj8dKGBhftBmplSMkDj&date=" + date;
+ 
+    $.ajax({
+        url: url
+    }).done(function(result){
+      $("#apodTitle").text(result.title);
+      $("#apodImg").attr("src", result.url).attr('alt', result.title);
+      $("#apodCopyright").text("Copyright: " + result.copyright);
+      $("#apodDate").text("Date: " + date);
+      $("#apodDesc").text(result.explanation);
+    }).fail(function(data){
+      console.log(data);
+    });
+  },
+};
+apod.init();
